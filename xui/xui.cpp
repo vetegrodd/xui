d80bbe5b62993537d6e483c934f008381ef3fdc0
Carl Cederberg <carl.cederberg@gmail.com>
2021-11-05 13:11:06 +0100

    Test UI

---
+++ xui/xui.cpp
@@ -0,0 +1,51 @@
+﻿
+#include <TGUI/TGUI.hpp>
+#include <iostream>
+
+void login(tgui::EditBox::Ptr username, tgui::EditBox::Ptr password)
+{
+    std::cout << "Username: " << username->getText().toAnsiString() << std::endl;
+    std::cout << "Password: " << password->getText().toAnsiString() << std::endl;
+}
+
+
+int main()
+{
+    // Create the window
+    sf::RenderWindow window(sf::VideoMode(400, 300), "TGUI window");
+    tgui::Gui gui(window);
+
+    try {
+        auto tabs = tgui::Tabs::create();
+        tabs->setTabHeight(30);
+        tabs->setPosition(70, 40);
+        tabs->add("Tab - 1");
+        tabs->add("Tab - 2");
+        tabs->add("Tab - 3");
+        gui.add(tabs);
+    }
+    catch (const tgui::Exception& e)
+    {
+        std::cerr << "TGUI Exception: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // Main loop
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+            gui.handleEvent(event);
+        }
+
+        window.clear();
+        gui.draw();
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
+}
