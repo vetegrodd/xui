54a640d32a946dbba3b6f8780b166fcd9dfc230c
Carl Cederberg <carl.cederberg@gmail.com>
2022-10-19 20:07:58 +0200

    More stuff

--- xui4/xui4.cpp
+++ xui4/xui4.cpp
@@ -2,17 +2,24 @@
 
 #include <nana/gui/widgets/label.hpp>
 #include <nana/gui/widgets/button.hpp>
+#include <nana/gui/widgets/combox.hpp>
 
 int main() {
 
 	using namespace nana;
 	form fm;
-	label lb{ fm, rectangle{ 10, 10, 100, 20 } };
-	lb.bgcolor(colors::blue);
-	lb.caption("Hello, world!");
+	label helloLabel{ fm, rectangle{ 10, 10, 100, 20 } };
+	helloLabel.bgcolor(colors::blue);
+	helloLabel.caption("Hello, world!");
 	button bn{ fm, rectangle{ 120, 10, 50, 29 } };
 	bn.caption("Press");
-	bn.events().click([&lb](const arg_click& arg) { lb.caption("clucked"); });
+	bn.events().click([&helloLabel](const arg_click& arg) { helloLabel.caption("clucked"); });
+
+	combox cb{ fm , rectangle{10, 30, 50, 20} };
+	cb.push_back("hej");
+	cb.push_back("hopp");
+	cb.push_back("ditt");
+
 	fm.show();
 	exec();
 }
